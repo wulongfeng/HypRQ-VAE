aa7f4f7074045a8431054099bb34d3773dfe3cfa
Longfeng Wu <wulongfeng1990@gmail.com>
2025-10-09 15:15:16 -0700

    Create train_tokenizer.sh

---
+++ train_tokenizer.sh
@@ -0,0 +1,6 @@
+python hyp_main.py 
+--data_path ../data/Instruments/Instruments.emb-llama-td.npy\
+--alpha 0.01 \
+--beta 0.0001 \
+--cf_emb ./RQ-VAE/ckpt/Instruments-32d-sasrec.pt\
+--ckpt_dir ../checkpoint/
