49001dd34221f52869e080819406b80a677509ac
Longfeng Wu <wulongfeng1990@gmail.com>
2025-10-10 10:20:54 -0700

    Update train_tokenizer.sh

--- train_tokenizer.sh
+++ train_tokenizer.sh
@@ -2,5 +2,4 @@ python hyp_main.py
 --data_path ../data/Instruments/Instruments.emb-llama-td.npy\
 --alpha 0.01 \
 --beta 0.0001 \
---cf_emb ./RQ-VAE/ckpt/Instruments-32d-sasrec.pt\
 --ckpt_dir ../checkpoint/
